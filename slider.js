dd4db7c8c0024a225190f93dec3daf1fba384c60
RangerRhino23 <ryadahlke@outlook.com>
2023-08-04 12:49:14 -0500

    wrote how to use comments

--- slider.js
+++ slider.js
@@ -1,3 +1,15 @@
+//slider.js Taptapir Utilities by RangerRhino23
+
+// How To Use
+
+// Setup: Before <script src="taptapir.js">, put <script src="taptapir-utilities/slider.js"></script>
+
+// Usage: In <script src="slider.js">, put var = new Slider(scale=[0.6,0.5],value=100,x=0,y=0)
+
+// How to get value: print(var.value)
+
+// Change Slider: var.setValue(value=100)
+
 class Slider extends Entity{
     constructor({scale=[.45,.05],value=10,x=0,y=0}) {
         super(function update(){});
@@ -16,5 +28,8 @@ class Slider extends Entity{
             this.sliderButton.x = this.sliderVal.scale_x/2-0;
         }
         this.value = round((this.sliderButton.x + this.sliderVal.scale_x/2) * 100 / this.sliderVal.scale_x, 0);
+    setValue = function(value=100) {
+        this.sliderButton.x = (this.sliderButton.x = value * this.sliderVal.scale_x / 100 - this.sliderVal.scale_x / 2);
+    }
     }
 }
