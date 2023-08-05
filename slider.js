7e8f5cae8b74714006c65afbec3d2793d052e778
RangerRhino23 <ryadahlke@outlook.com>
2023-08-04 19:36:14 -0500

    Added color changing support to Slider

--- slider.js
+++ slider.js
@@ -4,19 +4,21 @@
 
 // Setup: Before <script src="taptapir.js">, put <script src="taptapir-utilities/slider.js"></script>
 
-// Usage: In <script src="slider.js">, put var = new Slider(scale=[0.6,0.5],value=100,x=0,y=0)
+// Usage: In <script src="slider.js">, put var = new Slider({args:here})
+
+// Possible Args (scale:[.45,.05],value:50,y:0,background_color:color.white,button_color:color.blue)
 
 // How to get value: print(var.value)
 
 // Change Slider: var.setValue(value=100)
 
 class Slider extends Entity{
-    constructor({scale=[.45,.05],value=10,x=0,y=0}) {
+    constructor({scale=[.45,.05],value=10,y=0,background_color=color.white,button_color=color.blue}) {
         super(function update(){});
         this.alpha = 0;
-        this.sliderBackground = new Entity({scale:[scale[0]+scale[1],scale[1]],x:x,y:y,roundness:0.5});
-        this.sliderVal = new Entity({scale:[scale[0],0],x:x,y:y})
-        this.sliderButton = new Button({scale:[scale[1],scale[1]],x:x,y:y,roundness:0.5,color:color.blue,draggable:true,lock_y:true});
+        this.sliderBackground = new Entity({scale:[scale[0]+scale[1],scale[1]],x:0,y:y,roundness:0.5,color:background_color});
+        this.sliderVal = new Entity({scale:[scale[0],0],x:0,y:y})
+        this.sliderButton = new Button({scale:[scale[1],scale[1]],x:0,y:y,roundness:0.5,color:button_color,draggable:true,lock_y:true});
         this.value = value;
         this.sliderButton.x = (this.sliderButton.x = this.value * this.sliderVal.scale_x / 100 - this.sliderVal.scale_x / 2);
         }
